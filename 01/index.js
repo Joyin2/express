aa91811578c97df3c66e9cd1b68e969599751031
Joyin2 <joyinlaskar2000@gmail.com>
2022-10-16 23:20:47 +0530

    blogexpress

--- 01/index.js
+++ 01/index.js
@@ -3,8 +3,10 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello World!" + req.params.name);
 });
 app.get("/about", (req, res) => {
   // res.send("about");
---
+++ blogexpress/index.js
@@ -0,0 +1,589 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello World!" + req.params.name);
+});
+app.get("/about", (req, res) => {
+  // res.send("about");
+  // res.sendFile(path.join(__dirname, "index.html"));
+  res.json({ joyin: 22 });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello World!" + req.params.name);
+});
+app.get("/about", (req, res) => {
+  // res.send("about");
+  // res.sendFile(path.join(__dirname, "index.html"));
+  res.json({ joyin: 22 });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello World!" + req.params.name);
+});
+app.get("/about", (req, res) => {
+  // res.send("about");
+  // res.sendFile(path.join(__dirname, "index.html"));
+  res.json({ joyin: 22 });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello World!" + req.params.name);
+});
+app.get("/about", (req, res) => {
+  // res.send("about");
+  // res.sendFile(path.join(__dirname, "index.html"));
+  res.json({ joyin: 22 });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello World!" + req.params.name);
+});
+app.get("/about", (req, res) => {
+  // res.send("about");
+  // res.sendFile(path.join(__dirname, "index.html"));
+  res.json({ joyin: 22 });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello World!" + req.params.name);
+});
+app.get("/about", (req, res) => {
+  // res.send("about");
+  // res.sendFile(path.join(__dirname, "index.html"));
+  res.json({ joyin: 22 });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello World!" + req.params.name);
+});
+app.get("/about", (req, res) => {
+  // res.send("about");
+  // res.sendFile(path.join(__dirname, "index.html"));
+  res.json({ joyin: 22 });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello World!" + req.params.name);
+});
+app.get("/about", (req, res) => {
+  // res.send("about");
+  // res.sendFile(path.join(__dirname, "index.html"));
+  res.json({ joyin: 22 });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello World!" + req.params.name);
+});
+app.get("/about", (req, res) => {
+  // res.send("about");
+  // res.sendFile(path.join(__dirname, "index.html"));
+  res.json({ joyin: 22 });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello World!" + req.params.name);
+});
+app.get("/about", (req, res) => {
+  // res.send("about");
+  // res.sendFile(path.join(__dirname, "index.html"));
+  res.json({ joyin: 22 });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello World!" + req.params.name);
+});
+app.get("/about", (req, res) => {
+  // res.send("about");
+  // res.sendFile(path.join(__dirname, "index.html"));
+  res.json({ joyin: 22 });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello World!" + req.params.name);
+});
+app.get("/about", (req, res) => {
+  // res.send("about");
+  // res.sendFile(path.join(__dirname, "index.html"));
+  res.json({ joyin: 22 });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello World!" + req.params.name);
+});
+app.get("/about", (req, res) => {
+  // res.send("about");
+  // res.sendFile(path.join(__dirname, "index.html"));
+  res.json({ joyin: 22 });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello World!" + req.params.name);
+});
+app.get("/about", (req, res) => {
+  // res.send("about");
+  // res.sendFile(path.join(__dirname, "index.html"));
+  res.json({ joyin: 22 });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello World!" + req.params.name);
+});
+app.get("/about", (req, res) => {
+  // res.send("about");
+  // res.sendFile(path.join(__dirname, "index.html"));
+  res.json({ joyin: 22 });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello World!" + req.params.name);
+});
+app.get("/about", (req, res) => {
+  // res.send("about");
+  // res.sendFile(path.join(__dirname, "index.html"));
+  res.json({ joyin: 22 });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello World!" + req.params.name);
+});
+app.get("/about", (req, res) => {
+  // res.send("about");
+  // res.sendFile(path.join(__dirname, "index.html"));
+  res.json({ joyin: 22 });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello World!" + req.params.name);
+});
+app.get("/about", (req, res) => {
+  // res.send("about");
+  // res.sendFile(path.join(__dirname, "index.html"));
+  res.json({ joyin: 22 });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello World!" + req.params.name);
+});
+app.get("/about", (req, res) => {
+  // res.send("about");
+  // res.sendFile(path.join(__dirname, "index.html"));
+  res.json({ joyin: 22 });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello World!" + req.params.name);
+});
+app.get("/about", (req, res) => {
+  // res.send("about");
+  // res.sendFile(path.join(__dirname, "index.html"));
+  res.json({ joyin: 22 });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello World!" + req.params.name);
+});
+app.get("/about", (req, res) => {
+  // res.send("about");
+  // res.sendFile(path.join(__dirname, "index.html"));
+  res.json({ joyin: 22 });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello World!" + req.params.name);
+});
+app.get("/about", (req, res) => {
+  // res.send("about");
+  // res.sendFile(path.join(__dirname, "index.html"));
+  res.json({ joyin: 22 });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello World!" + req.params.name);
+});
+app.get("/about", (req, res) => {
+  // res.send("about");
+  // res.sendFile(path.join(__dirname, "index.html"));
+  res.json({ joyin: 22 });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello World!" + req.params.name);
+});
+app.get("/about", (req, res) => {
+  // res.send("about");
+  // res.sendFile(path.join(__dirname, "index.html"));
+  res.json({ joyin: 22 });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello World!" + req.params.name);
+});
+app.get("/about", (req, res) => {
+  // res.send("about");
+  // res.sendFile(path.join(__dirname, "index.html"));
+  res.json({ joyin: 22 });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello World!" + req.params.name);
+});
+app.get("/about", (req, res) => {
+  // res.send("about");
+  // res.sendFile(path.join(__dirname, "index.html"));
+  res.json({ joyin: 22 });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello World!" + req.params.name);
+});
+app.get("/about", (req, res) => {
+  // res.send("about");
+  // res.sendFile(path.join(__dirname, "index.html"));
+  res.json({ joyin: 22 });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello World!" + req.params.name);
+});
+app.get("/about", (req, res) => {
+  // res.send("about");
+  // res.sendFile(path.join(__dirname, "index.html"));
+  res.json({ joyin: 22 });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello World!" + req.params.name);
+});
+app.get("/about", (req, res) => {
+  // res.send("about");
+  // res.sendFile(path.join(__dirname, "index.html"));
+  res.json({ joyin: 22 });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello World!" + req.params.name);
+});
+app.get("/about", (req, res) => {
+  // res.send("about");
+  // res.sendFile(path.join(__dirname, "index.html"));
+  res.json({ joyin: 22 });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello World!" + req.params.name);
+});
+app.get("/about", (req, res) => {
+  // res.send("about");
+  // res.sendFile(path.join(__dirname, "index.html"));
+  res.json({ joyin: 22 });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
