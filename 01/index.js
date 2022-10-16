4dc6e14474e0819004b9439f44e4441cc9f8a883
Joyin2 <joyinlaskar2000@gmail.com>
2022-10-16 22:34:54 +0530

    add

--- 01/index.js
+++ 01/index.js
@@ -1,14 +1,17 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
+const express = require("express");
+const path = require("path");
+const app = express();
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/about", (req, res) => {
+  // res.send("about");
+  // res.sendFile(path.join(__dirname, "index.html"));
+  res.json({ joyin: 22 });
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
 });
